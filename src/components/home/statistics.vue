b2ca5760165d72d4b3c06aa48843ec89a6f4b55d
ttishappy <1589686086@qq.com>
2023-10-15 21:02:44 +0800

    ui更新

---
+++ src/components/home/statistics.vue
@@ -0,0 +1,53 @@
+<template>
+    <el-card shadow="hover" class="card">
+        <el-text style="zoom:0.9;">数说河畔</el-text>
+        <div style="height: 10px;"></div>
+        <el-row style="width: 100%;">
+        <el-col :span="6">
+            <el-statistic title="活跃用户" :value="268500" />
+        </el-col>
+        <el-col :span="6">
+            <el-statistic :value="138">
+            <template #title>
+                <div style="display: inline-flex; align-items: center">
+                男女比例
+                <el-icon style="margin-left: 4px" :size="12">
+                    <Male />
+                </el-icon>
+                </div>
+            </template>
+            <template #suffix>/100</template>
+            </el-statistic>
+        </el-col>
+        <el-col :span="6">
+            <el-statistic title="帖子总数" :value="172000" />
+        </el-col>
+        <el-col :span="6">
+            <el-statistic title="互动总数" :value="562">
+            <template #suffix>
+                <el-icon style="vertical-align: -0.125em">
+                <ChatLineRound />
+                </el-icon>
+            </template>
+            </el-statistic>
+        </el-col>
+        </el-row>
+    </el-card>
+  </template>
+  
+  <script lang="ts" setup>
+  import { ChatLineRound, Male } from '@element-plus/icons-vue'
+  </script>
+  
+  <style scoped>
+  .el-col {
+    text-align: center;
+  }
+  .card {
+    margin-top: 30px;
+    width: 83%;
+    border-radius: 10px;
+  }
+
+  </style>
+  
--- src/views/homeView.vue
+++ src/views/homeView.vue
@@ -7,24 +7,40 @@
             <el-row style="margin-bottom: 10px;" class="center">
               <el-text>UI Designed By</el-text>
               <div style="width: 10px;"></div>
-              <el-tag round effect="dark"  style="font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;">Kristian</el-tag>
+              <el-tag round effect="dark"  style="font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-color: white !important; background: linear-gradient(45deg,  rgb(255, 153, 243), rgb(38, 175, 235));">Kristian</el-tag>
             </el-row>
             <!-- <carousel style="margin-top: 150px;"></carousel> -->
-            <el-row>
-              <el-col :span="8">
-                <el-card shadow="hover" style="height: 130px; border-radius: 10px;  background: linear-gradient(to right, red, blue);"></el-card>
+            <el-row class="poster1">
+              <el-col :span="11">
+                <el-card shadow="hover" style="height: 150px; border-radius: 10px;  background: linear-gradient(45deg,  rgb(43, 150, 237), rgb(19, 195, 248));">
+                    <el-row style="margin-top:25px;">
+                      <el-text style=" color: rgba(255, 255, 255, 0.918); font-weight: 300; font-size: 19px;">欢迎来到</el-text>
+                      <img class="logo" src="@/assets/shplogo.png">
+                    </el-row>
+                    <el-row>
+                      <el-text style="margin-top:10px;color: rgba(255, 255, 255, 0.918); border: none !important; font-weight: 400; font-size: 23px;">专为<span style="font-weight:500;">UESTCer</span>打造的在线校园交流平台</el-text>
+                    </el-row>
+                </el-card>
               </el-col>
-              <el-col :span="16">
+              <el-col :span="9">
+                <el-card shadow="hover" style="height: 150px; border-radius: 10px; margin-left: 120px; background-image: url('src/assets/uestcPoster.png'); background-size: cover;">
+                  <!-- <img
+                    src="@/assets/uestc.png"
+                    class="image"
+                  /> -->
+                </el-card>
               </el-col>
             </el-row>
-            
-            
+            <el-row class="center">
+              <statistics></statistics>
+            </el-row>
         </el-col>
     </el-row>
 </template>
 
 <script setup>
 import navigator from "@/components/home/navigator.vue"
+import statistics from "@/components/home/statistics.vue"
 // import carousel from "@/components/home/carousel.vue"
 const name = 'HomeView';
 </script>
@@ -44,4 +60,28 @@ const name = 'HomeView';
     justify-content: center;
     align-items: center;
 }
+
+.logo {
+    width: 64px;
+    height:24px;
+    padding-left: 10px;
+    padding-top: 1px;
+  }
+
+
+  .image {
+    width: 200px;
+    height: 100px;
+    margin-top: 10px;
+    margin-left: 70px;
+    object-fit: cover;
+    display:block;
+  }
+  .poster1{
+    margin-top: 30px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
