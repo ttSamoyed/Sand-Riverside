4ec5a746a5926459d872d221b319764c97fe1ce2
faffaafafa <669567808@qq.com>
2023-11-09 13:45:19 +0800

    修改密码ing

--- src/components/mypage/setting_card.vue
+++ src/components/mypage/setting_card.vue
@@ -27,6 +27,7 @@
                     <avatar_upload :userid="user.userID"></avatar_upload>
                 </el-col> -->
             <el-divider></el-divider>
+
             <el-row>
                 <el-col span="8">
                     <el-text> 学院 </el-text>
@@ -41,11 +42,14 @@
                     <el-input v-model="user.college" placeholder="请输入学校"></el-input>
                 </el-col>
             </el-row>
+
             <el-divider></el-divider>
 
             <map_select :p="province" :c="city" :r="district" ref="mapselect" style="margin-left: -36px;"></map_select>
             <!-- 为啥这里改为map_select就不显示了 -->
-            <el-divider></el-divider><el-row>
+            <el-divider></el-divider>
+
+            <el-row>
                 <el-col span="8">
                     <el-text> 签名 </el-text>
                 </el-col>
@@ -53,22 +57,36 @@
                     <el-input v-model="user.status" style="width: 500px;" placeholder="请输入签名"></el-input>
                 </el-col>
             </el-row>
+
             <el-divider></el-divider>
+
             <el-row>
                 <div>
                 <el-button type="primary" plain round @click="updateInfo">修改信息</el-button>
                 <el-button type="primary" plain round @click="showPasswordDialog = true " >修改密码</el-button>
-                <el-dialog title="修改密码" width="30%" v-model="showPasswordDialog">
-                <span>请输入旧密码：</span>
-                <el-input v-model="oldPassword" type="password" placeholder="请输入旧密码"></el-input>
-                <br>
-                <span>请输入新密码：</span>
-                <el-input v-model="newPassword" type="password" placeholder="请输入新密码"></el-input>
-                <br>
-                <span slot="footer" class="dialog-footer">
-                <el-button @click="showPasswordDialog = false">取消</el-button>
-                <el-button type="primary" @click="updatePassword">修改</el-button>
-                </span>
+                <el-dialog 
+                  v-model="showPasswordDialog"
+                  title="修改密码" 
+                  width="30%"
+                  :before-close="handleClose"
+                  custom-class="password-dialog"
+                >
+                    <div class="input-section">
+                        <span class="input-label">请输入旧密码：</span>
+                        <el-input v-model="input_oldPassword" type="password" placeholder="请输入旧密码"></el-input>
+                    </div>
+                    <div class="input-section">
+                        <span class="input-label">请输入新密码：</span>
+                        <el-input v-model="newPassword" type="password" placeholder="请输入新密码"></el-input>
+                    </div>
+                    <div class="input-section">
+                        <span class="input-label">请再次输入新密码：</span>
+                        <el-input v-model="confirmPassword" type="password" placeholder="请输入再次新密码"></el-input>
+                    </div>
+                    <span slot="footer" class="dialog-footer">
+                        <el-button @click="showPasswordDialog = false">取消</el-button>
+                        <el-button type="primary" @click="updatePassword">修改</el-button>
+                    </span>
                 </el-dialog>
                 </div>
             </el-row>
@@ -109,12 +127,21 @@ const user = ref({
 })
 const address = ref('北京市-北京市-朝阳区')
 
+//获取个人信息
+for (var i = 0; i < localStorage.length; i++){
+    var key = localStorage.key(i);
+    var value = localStorage.getItem(key);
+    console.log("现在在第"+i+"次遍历localstorage");
+    console.log(key, value);
+}
 const getPersonalInfo = async () => {
     const status = localStorage.getItem('status');
     if (status) {
         loading.value = false;
         const u = JSON.parse(localStorage.getItem('user'));
-        console.log('userID:', u.userID)
+        console.log('userID:', u.userID);
+        console.log('u里面的信息为：', u);
+
         const response = await DataService.Get_Personal_Info(u.userID);
         if (response.data.status === 'failed') {
             console.log('status=',response.data.status)
@@ -128,7 +155,7 @@ const getPersonalInfo = async () => {
             console.log('userinfonew:', user.value)
             console.log('username:',user.value.username)
         }
-        return;
+        return u;
     }
     else {
         ElMessage.error('您还没有登录，请先登录！');
@@ -137,25 +164,22 @@ const getPersonalInfo = async () => {
 };
 
 
-
+//处理户籍信息
 const areas = computed(() => {
   return (address.value || '').split('-') // 使用 split 方法分割字符串，并提供一个空字符串作为默认值
 })
-
 const province = computed(() => {
   return areas.value[0] // 获取省份名称
 })
-
 const city = computed(() => {
   return areas.value[1] // 获取城市名称
 })
-
 const district = computed(() => {
   return areas.value[2] // 获取区县名称
 })
 
+//修改个人信息
 const mapselect=ref();
-
 const updateInfo = async () => {
     console.log('user=',user.value)
     console.log('userid=',user.value.userID)
@@ -189,31 +213,35 @@ const updateInfo = async () => {
     // }
 }
 
+
 const showPasswordDialog = ref(false);
+const input_oldPassword = ref('') 
+const newPassword = ref('') 
+const confirmPassword = ref('')
 
-const    oldPassword=ref('') 
-const    newPassword=ref('') 
-// console.log("dialog=",showPasswordDialog.value);
-//     const oldPassword = ref('');
-//     const newPassword = ref('');
-//     const confirmPassword = ref('');
+const updatePassword = async() => {
+    const u = await getPersonalInfo(); // 调用getPersonalInfo函数以获取u的值
+    if (u && u.userID != null) {
+        console.log("3534535353"+u);
+    } else {
+        ElMessage({
+        type: 'error',
+        message: '您还没有登录，请先登录！',
+    });
+    } 
 
-//     const update = () => {
-//       // 更新信息的代码
-//     };
-
-//     const updatePassword = () => {
-//       if (newPassword.value !== confirmPassword.value) {
-//         alert('两次输入的新密码不一致');
-//         return;
-//       }
-//       if (oldPassword.value === '正确的旧密码') { // 这里需要替换为实际的验证旧密码的逻辑
-//         alert('修改成功');
-//         showPasswordDialog.value = false;
-//       } else {
-//         alert('旧密码错误');
-//       }
-//     };
+        if (newPassword.value !== confirmPassword.value) {
+            alert('两次输入的新密码不一致');
+            return;
+        }
+        else if (input_oldPassword.value !== '正确的旧密码') { // 这里需要替换为实际的验证旧密码的逻辑
+            alert('旧密码错误');
+            
+        } else {
+            alert('修改成功');
+            showPasswordDialog.value = false;
+        }
+    };
 
 
  onMounted(getPersonalInfo)
@@ -240,6 +268,20 @@ const    newPassword=ref('')
 .info{
     margin-top: 20px;
     width: 700px;
+}
+.password-dialog .input-section {
+  margin-bottom: 20px;
+}
+
+.password-dialog .input-label {
+  display: block;
+  margin-bottom: 10px;
+  font-weight: bold;
+}
+
+.password-dialog .dialog-footer {
+  display: flex;
+  justify-content: flex-end;
 }
 
 </style>
